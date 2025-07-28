757aec5625cf07fcdfcd4840148ca13c65d0c635
JheraldC <jcaceresap@unsa.edu.pe>
2025-07-28 10:36:40 -0500

    nombre

--- backend/as.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-print("Cargando tokenizer...")
-tokenizer = AutoTokenizer.from_pretrained("google/medgemma-4b-it")
-
-print("Cargando modelo...")
-model = AutoModelForCausalLM.from_pretrained("google/medgemma-4b-it")
-
-print("Modelo cargado correctamente")
